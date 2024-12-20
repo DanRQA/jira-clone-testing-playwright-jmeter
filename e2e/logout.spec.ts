58318abedd316e187139181f7a668d7f37aa4235
Daniel Ramos <1979ramosdan@gmail.com>
2024-12-20 07:40:15 -0800

    Enhance login functionality and add logout test

---
+++ e2e/logout.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect, Page } from "@playwright/test";
+import { ProjectsPage } from "../pom/projectsPage";
+import { login } from "../api/loginApi";
+import { LoginPage } from "../pom/loginPage";
+
+test.beforeEach(async ({ page, baseURL }: { page: Page; baseURL?: string }) => {
+  if (!baseURL) {
+    throw new Error("baseURL is not defined");
+  }
+  await login(page, baseURL);
+
+  const projectsPage = new ProjectsPage(page);
+  await test.step("User is logged in via api and starts in the Projects Page", async () => {
+    await expect(page).toHaveURL(projectsPage.getProjectsPageUrl());
+  });
+});
+
+test("User is able to logout", async ({ page }: { page: Page }) => {
+  const userInitials = "ds";
+  const loginPage = new LoginPage(page);
+  const projectsPage = new ProjectsPage(page);
+  await test.step("User clicks on logout button", async () => {
+    await projectsPage.getUserAvatarButton(userInitials).click();
+    await projectsPage.getLogoutButton().click();
+  });
+
+  await test.step("User is back at login screen", async () => {
+    await expect(page).toHaveURL(loginPage.getLoginPageUrl());
+    await expect(loginPage.getLoginButton()).toBeVisible();
+  });
+});
--- pom/loginPage.ts
+++ pom/loginPage.ts
@@ -12,7 +12,7 @@ export class LoginPage {
   }
 
   getLoginPageUrl() {
-    return "/";
+    return "/login";
   }
 
   getLoginPageHeader() {
