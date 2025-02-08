2388119f490acddbeefc68fb48a399e4d045005e
DRamos <dramos.qa@gmail.com>
2025-02-08 07:21:05 -0800

    Env variable setup github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "http://app:3000", // Use the service name 'app' instead of 'localhost'
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
